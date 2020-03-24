ca2e0484b5aefa2ecf19d797b60fa4ce243f8361
Mai Yanagimura <yanagimura@sencorp.co.jp>
2020-03-24 11:07:06 +0900

    文末の改行がなかったので

--- laravel/resources/js/js-todo-list/lib.js
+++ laravel/resources/js/js-todo-list/lib.js
@@ -1,12 +1,12 @@
 export default {
-    /**
-     * 子要素を全部削除
-     *
-     * @param {HTMLElement}
-     */
-    removeAllChild(elem) {
-      while (elem.firstChild) {
-        elem.removeChild(elem.firstChild);
-      }
+  /**
+   * 子要素を全部削除
+   *
+   * @param {HTMLElement}
+   */
+  removeAllChild(elem) {
+    while (elem.firstChild) {
+      elem.removeChild(elem.firstChild);
     }
   }
+}
