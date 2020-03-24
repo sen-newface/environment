576ca735ca9238881ef34c9b495bd68622c330ae
Mai Yanagimura <yanagimura@sencorp.co.jp>
2020-03-24 10:54:36 +0900

    TODOリスト画面を表示できるように

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/js-todo-list', function () {
+    return view('js-todo-list');
+});
